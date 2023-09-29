641b76e0305f85cc197468269772fd7a563c99b6
Michael Ibuaku <mujiangelo17@gmail.com>
2023-09-29 16:33:58 +0300

    go

--- src/app/components/form.js
+++ src/app/components/form.js
@@ -4,6 +4,7 @@ import React from 'react';
 import {
   Flex,
   Box,
+  Img,
   FormControl,
   FormLabel,
   Input,
@@ -14,51 +15,54 @@ import {
   Text,
   useColorModeValue,
 } from '@chakra-ui/react';
+import Link from "next/link";
 
 export default function Form() {
   return (
+    <>
+    <Link href={'../'}><Img src='/logo.png' /></Link>
     <Flex
-      minH={'100vh'}
-      align={'center'}
-      justify={'center'}
-      bg={useColorModeValue('gray.50', 'gray.800')}
-    >
-      <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
-        <Stack align={'center'}>
-          <Heading fontSize={'4xl'}>Log in or Sign up</Heading>
-          <Text fontSize={'lg'} color={'gray.600'}>
-            Join millions of others in sharing succesful moves on  <Text color={'blue.400'}>HelpMeOut</Text> ✌️
-          </Text>
-        </Stack>
-        <Box
-          rounded={'lg'}
-          bg={useColorModeValue('white', 'gray.700')}
-          boxShadow={'lg'}
-          p={8}
-        >
-          <Stack spacing={4}>
-            <FormControl id="email">
-              <FormLabel>Email </FormLabel>
-              <Input type="email" placeholder='Enter your email address' />
-            </FormControl>
-            <FormControl id="password">
-              <FormLabel>Password</FormLabel>
-              <Input type="password" placeholder='Enter your password' />
-            </FormControl>
-            <Stack spacing={10}>
-              <Button
-                bg={'#100A42'}
-                color={'white'}
-                _hover={{
-                  bg: 'blue.500',
-                }}
+          minH={'100vh'}
+          align={'center'}
+          justify={'center'}
+          bg={useColorModeValue('gray.50', 'gray.800')}
+      >
+          <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+              <Stack align={'center'}>
+                  <Heading fontSize={'4xl'}>Log in or Sign up</Heading>
+                  <Text fontSize={'lg'} color={'gray.600'}>
+                      Join millions of others in sharing succesful moves on  <Text color={'blue.400'}>HelpMeOut</Text> ✌️
+                  </Text>
+              </Stack>
+              <Box
+                  rounded={'lg'}
+                  bg={useColorModeValue('white', 'gray.700')}
+                  boxShadow={'lg'}
+                  p={8}
               >
-                Sign in
-              </Button>
-            </Stack>
+                  <Stack spacing={4}>
+                      <FormControl id="email">
+                          <FormLabel>Email </FormLabel>
+                          <Input type="email" placeholder='Enter your email address' />
+                      </FormControl>
+                      <FormControl id="password">
+                          <FormLabel>Password</FormLabel>
+                          <Input type="password" placeholder='Enter your password' />
+                      </FormControl>
+                      <Stack spacing={10}>
+                          <Button
+                              bg={'#100A42'}
+                              color={'white'}
+                              _hover={{
+                                  bg: 'blue.500',
+                              }}
+                          >
+                              Sign in
+                          </Button>
+                      </Stack>
+                  </Stack>
+              </Box>
           </Stack>
-        </Box>
-      </Stack>
-    </Flex>
+      </Flex></>
   );
 }
