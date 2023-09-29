07179a153fe53482c7208cb5835966f3fecd5768
Michael Ibuaku <mujiangelo17@gmail.com>
2023-09-29 22:00:52 +0300

    boom

--- src/app/components/ready.js
+++ src/app/components/ready.js
@@ -1,7 +1,7 @@
 'use client'
 
 import { Stack, Box, Text, Heading, Img, Input, InputGroup, InputRightElement, Button } from '@chakra-ui/react';
-import { EditIcon } from '@chakra-ui/icons';
+import { EditIcon, CopyIcon } from '@chakra-ui/icons';
 import React from 'react';
 const Ready = () => {
     return (
@@ -11,11 +11,11 @@ const Ready = () => {
             pl={'50px'}
             pr={'50px'}
         >
-            <Box m={'auto'}>
-                <Heading color={'#141414'} size={'md'} mb={'50px'}>Your video is ready!</Heading>
+            <Box>
+                <Heading color={'#141414'} size={'lg'} mb={'20px'}>Your video is ready!</Heading>
 
                 <Text fontSize={'15px'}>Name </Text>
-                <Text color={'#413C6D'} fontWeight={'bold'}>Untitled_Video_20232509 <EditIcon color={'#413C6D'} /> </Text>
+                <Text color={'#413C6D'} fontWeight={'bold'} mb={'50px'}>Untitled_Video_20232509 <EditIcon color={'#413C6D'} /> </Text>
 
                 {/** enter email */}
                 <InputGroup size='md' mb={'50px'}>
@@ -23,6 +23,7 @@ const Ready = () => {
                         pr='4.5rem'
                         type='email'
                         placeholder='enter email of receiver'
+                        w={'90%'}
                     />
                     <InputRightElement width='4.5rem'>
                         <Button h='1.75rem' size='sm' bg={'#605C84'}>
@@ -30,8 +31,26 @@ const Ready = () => {
                         </Button>
                     </InputRightElement>
                 </InputGroup>
+                
+                {/** video url */}
+                <Heading color={'#141414'} size={'md'}>Video Url</Heading>
+                <InputGroup size='md' mb={'50px'}>
+                    <Input
+                        pr='4.5rem'
+                        type='email'
+                        placeholder='https://www.helpmeout/Untitled_Video_20232509'
+                        w={'90%'}
+                    />
+                    <InputRightElement width='4.5rem'>
+                        <Button h='1.75rem' size='sm' borderColor={'#605C84'} bg={'white'}>
+                            <CopyIcon /> Copy
+                        </Button>
+                    </InputRightElement>
+                </InputGroup>
+
+
             </Box>
-            <Box m={'auto'}></Box>
+            <Box></Box>
         </Stack>
     );
 }
