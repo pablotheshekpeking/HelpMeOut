236bbaa80f0299ef3d4b3a636b923f96de511da9
Michael Ibuaku <mujiangelo17@gmail.com>
2023-09-29 21:49:10 +0300

    lets see

--- src/app/components/ready.js
+++ src/app/components/ready.js
@@ -1,25 +1,39 @@
 'use client'
 
-import { Stack, Box, Text, Heading, Img } from '@chakra-ui/react';
+import { Stack, Box, Text, Heading, Img, Input, InputGroup, InputRightElement, Button } from '@chakra-ui/react';
 import { EditIcon } from '@chakra-ui/icons';
 import React from 'react';
 const Ready = () => {
-    return ( 
+    return (
         <Stack
-        direction={['column', 'column', 'row', 'row']}
-        minH={'100vh'}
-        pl={'50px'}
-        pr={'50px'}
+            direction={['column', 'column', 'row', 'row']}
+            minH={'100vh'}
+            pl={'50px'}
+            pr={'50px'}
         >
-            <Box>
-                <Heading color={'#141414'} mb={'50px'}>Your video is ready!</Heading>
+            <Box m={'auto'}>
+                <Heading color={'#141414'} size={'md'} mb={'50px'}>Your video is ready!</Heading>
 
-                <Text fontSize={'11px'}>Name </Text>
-                <Text color={'#413C6D'}>Untitled_Video_20232509 </Text> <EditIcon color={'#413C6D'} />
+                <Text fontSize={'15px'}>Name </Text>
+                <Text color={'#413C6D'} fontWeight={'bold'}>Untitled_Video_20232509 <EditIcon color={'#413C6D'} /> </Text>
+
+                {/** enter email */}
+                <InputGroup size='md' mb={'50px'}>
+                    <Input
+                        pr='4.5rem'
+                        type='email'
+                        placeholder='enter email of receiver'
+                    />
+                    <InputRightElement width='4.5rem'>
+                        <Button h='1.75rem' size='sm' bg={'#605C84'}>
+                            Send
+                        </Button>
+                    </InputRightElement>
+                </InputGroup>
             </Box>
-            <Box></Box>
+            <Box m={'auto'}></Box>
         </Stack>
-     );
+    );
 }
- 
+
 export default Ready;
