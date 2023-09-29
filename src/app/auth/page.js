5fd912a523ac025edf11ae0eabe1ec6636c5dc28
Michael Ibuaku <mujiangelo17@gmail.com>
2023-09-29 11:30:44 +0300

    first commit

--- src/app/auth/page.js
+++ src/app/auth/page.js
@@ -1,9 +1,9 @@
 import Image from 'next/image'
 import styles from './page.module.css'
-import { Providers } from './provider'
+import { Providersa } from './providera'
 
 export default function Home() {
   return (
-    <Providers />
+    <Providersa />
   )
 }
---
+++ src/app/components/navbar.js
@@ -0,0 +1,34 @@
+'use client'
+import { Stack, Box, Img, Text, Heading } from "@chakra-ui/react";
+import Link from "next/link";
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <Stack direction={'row'} p={'20px'} m={'auto'}>
+            {/** logo */}
+            <Box w={'25%'}>
+                <Link href={'./'}><Img src="/logo.png" /></Link>
+            </Box>
+
+            {/** nav items */}
+            <Box m={'auto'} w={'50%'}>
+                <Stack direction={'row'} m={'auto'}>
+                    <Box w={'25%'} m={'auto'}>
+                        <Text color={'#100A42'}>Features</Text>
+                    </Box>
+                    <Box w={'25%'} m={'auto'}>
+                        <Text color={'#100A42'}>How It Works</Text>
+                    </Box>
+                </Stack>
+            </Box>
+
+            {/** login link */}
+            <Box m={'auto'} w={'25%'}>
+                <Link href={'./auth'}><Heading size='xs' color={'#100A42'} textAlign={'center'} float={'right'}>Get Started</Heading></Link>
+            </Box>
+        </Stack>
+    );
+}
+
+export default Navbar;
