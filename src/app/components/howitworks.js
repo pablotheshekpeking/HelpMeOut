e7ef45af26bb471d8fbfc88fc6b2251be17d2988
Michael Ibuaku <mujiangelo17@gmail.com>
2023-09-30 21:49:00 +0300

    again

--- src/app/components/howitworks.js
+++ src/app/components/howitworks.js
@@ -14,7 +14,7 @@ const HowItWorks = () => {
         {/** content */}
             <Stack 
             direction={['column', 'column', 'row', 'row']}
-            m={'auto'}
+            justify={'center'}
             pr={'50px'}
             pl={'50px'}
             pb={'50px'}
