fe89ad0a69c58b3c7e7c7d18c9fba8faf47874d7
Michael Ibuaku <mujiangelo17@gmail.com>
2023-09-30 12:46:56 +0300

    5

--- src/app/components/createaccount.js
+++ src/app/components/createaccount.js
@@ -7,7 +7,7 @@ const Create = () => {
         <Stack bg={'rgba(231, 231, 237, 0.20)'} mt={'50px'} mb={'50px'}>
             <Box textAlign={'center'} m={'auto'} pt={'50px'} pb={'50px'}>
                 <Text fontWeight={'bold'} color={'#141414'}>To ensure the availability and privacy of your video, we recommend saving it to your account.</Text>
-                <Button bg={'#120B48'} p={'20px'} mt={'20px'} mb={'20px'} color={'white'}>
+                <Button bg={'#120B48'} p={'20px'} mt={'40px'} mb={'40px'} color={'white'} w={'90px'}>
                     Send
                 </Button>
                 <Text color={'#7E7E7E'}>Don’t have an account? <span style={{color: '#120B48',}}>Create account</span></Text> 
