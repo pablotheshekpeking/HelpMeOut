1e78d4d75940f7fc50f491ba5a7cec2ebd30f7f2
Michael Ibuaku <mujiangelo17@gmail.com>
2023-09-30 13:03:19 +0300

    lets see

--- src/app/components/createaccount.js
+++ src/app/components/createaccount.js
@@ -2,19 +2,33 @@
 
 import React from "react";
 import { Stack, Box, Text, Button } from '@chakra-ui/react';
+import {
+    Popover, PopoverTrigger, PopoverContent, PopoverHeader, PopoverBody, PopoverFooter, PopoverArrow, PopoverCloseButton, PopoverAnchor,
+} from '@chakra-ui/react'
 import Link from 'next/link';
 const Create = () => {
-    return ( 
+    return (
         <Stack bg={'rgba(231, 231, 237, 0.20)'} mt={'50px'} mb={'50px'}>
             <Box textAlign={'center'} m={'auto'} pt={'50px'} pb={'50px'}>
                 <Text fontWeight={'bold'} color={'#141414'}>To ensure the availability and privacy of your video, we recommend saving it to your account.</Text>
-                <Button bg={'#120B48'} p={'20px'} mt={'40px'} mb={'40px'} color={'white'} w={'90px'}>
-                    Send
-                </Button>
-                <Text fontWeight={'bold'} color={'#7E7E7E'}>Don’t have an account? <Link href={'../login'}><span style={{color: '#120B48', textDecorationLine: 'underline',}}>Create account</span></Link></Text> 
+
+                <Popover>
+                    <PopoverTrigger>
+                        <Button bg={'#120B48'} p={'20px'} mt={'40px'} mb={'40px'} color={'white'} w={'90px'}>
+                            Send
+                        </Button>
+                    </PopoverTrigger>
+                    <PopoverContent>
+                        <PopoverArrow />
+                        <PopoverCloseButton />
+                        <PopoverHeader>Confirmation!</PopoverHeader>
+                        <PopoverBody>Are you sure you want to have that milkshake?</PopoverBody>
+                    </PopoverContent>
+                </Popover>
+                <Text fontWeight={'bold'} color={'#7E7E7E'}>Don’t have an account? <Link href={'../login'}><span style={{ color: '#120B48', textDecorationLine: 'underline', }}>Create account</span></Link></Text>
             </Box>
         </Stack>
-     );
+    );
 }
- 
+
 export default Create;
