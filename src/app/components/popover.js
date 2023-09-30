6001903408ba22fa0b630e2f1424c225e2f47ce0
Michael Ibuaku <mujiangelo17@gmail.com>
2023-09-30 13:22:52 +0300

    2

--- src/app/components/popover.js
+++ src/app/components/popover.js
@@ -11,11 +11,11 @@ const Popover1 = () => {
                 <Text textAlign={'center'}>Your video link has been sent to <span style={{ color: '#120B48', }}>johnmark@gmail.com</span></Text>
             </Box>
             <Box textAlign={'center'} m={'auto'} pt={'50px'} pb={'50px'}>
-                <Text fontWeight={'bold'} color={'#141414'} textAlign={'center'}>Would you need to view this video later? Save to your account now!</Text>
-                <Button bg={'#120B48'} p={'20px'} mt={'40px'} mb={'40px'} color={'white'} w={'90px'}>
+                <Text color={'#141414'} textAlign={'center'}>Would you need to view this video later? Save to your account now!</Text>
+                <Button bg={'#120B48'} p={'30px'} mt={'40px'} mb={'40px'} color={'white'} w={'90px'}>
                     Save Video
                 </Button>
-                <Text fontWeight={'bold'} color={'#7E7E7E'} textAlign={'center'}>Don’t have an account? <Link href={'../login'}><span style={{ color: '#120B48', }}>Create account</span></Link></Text>
+                <Text color={'#7E7E7E'} textAlign={'center'}>Don’t have an account? <Link href={'../login'}><span style={{ color: '#120B48', }}>Create account</span></Link></Text>
             </Box>
         </Stack>
     );
