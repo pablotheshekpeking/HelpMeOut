6965435278e716402d1bb8a429b943d8ddbd541a
Michael Ibuaku <mujiangelo17@gmail.com>
2023-09-30 13:24:53 +0300

    again

--- src/app/components/popover.js
+++ src/app/components/popover.js
@@ -12,7 +12,7 @@ const Popover1 = () => {
             </Box>
             <Box textAlign={'center'} m={'auto'} pt={'50px'} pb={'50px'}>
                 <Text color={'#141414'} textAlign={'center'}>Would you need to view this video later? Save to your account now!</Text>
-                <Button bg={'#120B48'} p={'30px'} mt={'40px'} mb={'40px'} color={'white'} w={'90px'}>
+                <Button bg={'#120B48'} p={'20px'} mt={'40px'} mb={'40px'} color={'white'}>
                     Save Video
                 </Button>
                 <Text color={'#7E7E7E'} textAlign={'center'}>Don’t have an account? <Link href={'../login'}><span style={{ color: '#120B48', }}>Create account</span></Link></Text>
