07e604f9ed85ad1f7c69f03c13063d3203481ea2
Michael Ibuaku <mujiangelo17@gmail.com>
2023-09-30 20:34:25 +0300

    4

--- src/app/components/footer.js
+++ src/app/components/footer.js
@@ -8,29 +8,29 @@ const Footer = () => {
             <Box w={'40%'}>
                 <Img src='footerLogo.png' />
             </Box>
-            <Box w={'60%'} color={'white'} fontSize={['11px', '11px', '15px', '15px']}>
+            <Box w={'60%'} color={'white'} fontSize={['8px', '8px', '15px', '15px']}>
                 <Stack direction={'row'}>
                     <Box className='Menu' w={'33.3%'}>
                         <Stack direction={'column'}>
-                            <Box mb={'20px'}><Text fontWeight={'bold'}>Menu</Text></Box>
-                            <Box mb={'20px'}><Text>Home</Text></Box>
-                            <Box mb={'20px'}><Text>Converter</Text></Box>
+                            <Box mb={['10px', '10px', '20px', '20px']}><Text fontWeight={'bold'}>Menu</Text></Box>
+                            <Box mb={['10px', '10px', '20px', '20px']}><Text>Home</Text></Box>
+                            <Box mb={['10px', '10px', '20px', '20px']}><Text>Converter</Text></Box>
                             <Box><Text>How It Works</Text></Box>
                         </Stack>
                     </Box>
                     <Box className='About us' w={'33.3%'}>
                         <Stack direction={'column'}>
-                            <Box mb={'20px'}><Text fontWeight={'bold'}>About us</Text></Box>
-                            <Box mb={'20px'}><Text>About</Text></Box>
-                            <Box mb={'20px'}><Text>Contact us</Text></Box>
+                            <Box mb={['10px', '10px', '20px', '20px']}><Text fontWeight={'bold'}>About us</Text></Box>
+                            <Box mb={['10px', '10px', '20px', '20px']}><Text>About</Text></Box>
+                            <Box mb={['10px', '10px', '20px', '20px']}><Text>Contact us</Text></Box>
                             <Box><Text>Privacy Policy</Text></Box>
                         </Stack>
                     </Box>
                     <Box className='Screen Record' w={'33.3%'}>
                         <Stack direction={'column'}>
-                            <Box mb={'20px'}><Text fontWeight={'bold'}>Screen Record</Text></Box>
-                            <Box mb={'20px'}><Text>Browser Window</Text></Box>
-                            <Box mb={'20px'}><Text>Desktop</Text></Box>
+                            <Box mb={['10px', '10px', '20px', '20px']}><Text fontWeight={'bold'}>Screen Record</Text></Box>
+                            <Box mb={['10px', '10px', '20px', '20px']}><Text>Browser Window</Text></Box>
+                            <Box mb={['10px', '10px', '20px', '20px']}><Text>Desktop</Text></Box>
                             <Box><Text>Application</Text></Box>
                         </Stack>
                     </Box>
