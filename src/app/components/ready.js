7357ee9688e658628859aa26ea5a6fb3f7a34f48
Michael Ibuaku <mujiangelo17@gmail.com>
2023-10-01 17:08:58 +0300

    lets see

--- src/app/components/ready.js
+++ src/app/components/ready.js
@@ -1,71 +1,136 @@
 'use client'
 
-import { Stack, Box, Text, Heading, Img, Input, InputGroup, InputRightElement, Button, Divider } from '@chakra-ui/react';
+import React, { useState, useEffect } from 'react';
+import {
+  Stack,
+  Box,
+  Text,
+  Heading,
+  Img,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Button,
+  Divider,
+} from '@chakra-ui/react';
 import { EditIcon, CopyIcon } from '@chakra-ui/icons';
-import React from 'react';
+
 const Ready = () => {
-    return (
-        <Stack
-            direction={['column', 'column', 'row', 'row']}
-            minH={'100vh'}
-            p={'50px'}
-        >
-            {/** left side */}
-            <Box w={['100%', '1005', '50%', '50%']}>
-                <Heading color={'#141414'} size={'lg'} mb={'20px'}>Your video is ready!</Heading>
+  const [videoUrl, setVideoUrl] = useState('');
+  const [loading, setLoading] = useState(true);
 
-                <Text fontSize={'15px'}>Name </Text>
-                <Text color={'#413C6D'} fontWeight={'bold'} mb={'50px'}>Untitled_Video_20232509 <EditIcon color={'#413C6D'} /> </Text>
+  useEffect(() => {
+    // Fetch the video from the provided endpoint
+    fetch('https://chrome-extension-2njn.onrender.com/videoupload/')
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.blob();
+      })
+      .then((videoBlob) => {
+        // Create a video URL from the fetched video blob
+        const url = URL.createObjectURL(videoBlob);
+        setVideoUrl(url);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error('Error fetching video:', error);
+        setLoading(false);
+      });
+  }, []);
 
-                {/** enter email */}
-                <InputGroup size='md' mb={'50px'} w={'90%'}>
-                    <Input
-                        pr='4.5rem'
-                        type='email'
-                        placeholder='enter email of receiver'
-                    />
-                    <InputRightElement width='4.5rem'>
-                        <Button h='1.75rem' size='sm' bg={'#605C84'}>
-                            Send
-                        </Button>
-                    </InputRightElement>
-                </InputGroup>
-                
-                {/** video url */}
-                <Heading color={'#141414'} size={'sm'} mb={'10px'}>Video Url</Heading>
-                <InputGroup size='md' mb={'50px'} w={'90%'}>
-                    <Input
-                        pr='4.5rem'
-                        type='email'
-                        placeholder='https://www.helpmeout/Untitled_Video_20232509'
-                    />
-                    <InputRightElement width='4.5rem'>
-                        <Button h='1.75rem' size='sm' border={'1px solid'} borderColor={'#605C84'} bg={'white'} color={'#605C84'}>
-                            <CopyIcon color={'#605C84'} /> Copy
-                        </Button>
-                    </InputRightElement>
-                </InputGroup>
+  return (
+    <Stack
+      direction={['column', 'column', 'row', 'row']}
+      minH={'100vh'}
+      p={'50px'}
+    >
+      {/* Left side */}
+      <Box w={['100%', '1005', '50%', '50%']}>
+        <Heading color={'#141414'} size={'lg'} mb={'20px'}>
+          Your video is ready!
+        </Heading>
 
-                {/** share your video */}
-                <Heading color={'#141414'} size={'sm'} mb={'10px'}>Share your video</Heading>
-                <Stack direction={'row'} w={'75%'}>
-                    <Box><Img src='/fbbutton.png' /></Box>
-                    <Box><Img src='/whbutton.png' /></Box>
-                    <Box><Img src='/telbutton.png' /></Box>
-                </Stack>
+        <Text fontSize={'15px'}>Name </Text>
+        <Text color={'#413C6D'} fontWeight={'bold'} mb={'50px'}>
+          Untitled_Video_20232509 <EditIcon color={'#413C6D'} />{' '}
+        </Text>
 
+        {/* Enter email */}
+        <InputGroup size="md" mb={'50px'} w={'90%'}>
+          <Input
+            pr="4.5rem"
+            type="email"
+            placeholder="enter email of receiver"
+          />
+          <InputRightElement width="4.5rem">
+            <Button h="1.75rem" size="sm" bg={'#605C84'}>
+              Send
+            </Button>
+          </InputRightElement>
+        </InputGroup>
 
-            </Box>
-            
-            {/** right side */}
-            <Box w={['100%', '100%', '50%', '50%']} borderLeft={['none', 'none', '2px solid', '2px solid']} borderColor={'#BBBBBB'}>
-                <Stack direction={'column'} p={['', '', '40px', '40px']}>
-                    <Box mb={'50px'}><Img src='/video.png' /></Box>
-                    <Box><Img src='/transcript.png' /></Box>
-                </Stack>
-            </Box>
+        {/* Video url */}
+        <Heading color={'#141414'} size={'sm'} mb={'10px'}>
+          Video Url
+        </Heading>
+        <InputGroup size="md" mb={'50px'} w={'90%'}>
+          <Input
+            pr="4.5rem"
+            type="email"
+            placeholder="https://www.helpmeout/Untitled_Video_20232509"
+          />
+          <InputRightElement width="4.5rem">
+            <Button
+              h="1.75rem"
+              size="sm"
+              border={'1px solid'}
+              borderColor={'#605C84'}
+              bg={'white'}
+              color={'#605C84'}
+            >
+              <CopyIcon color={'#605C84'} /> Copy
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+
+        {/* Share your video */}
+        <Heading color={'#141414'} size={'sm'} mb={'10px'}>
+          Share your video
+        </Heading>
+        <Stack direction={'row'} w={'75%'}>
+          <Box>
+            <Img src="/fbbutton.png" />
+          </Box>
+          <Box>
+            <Img src="/whbutton.png" />
+          </Box>
+          <Box>
+            <Img src="/telbutton.png" />
+          </Box>
         </Stack>
-    );
-}
+      </Box>
+
+      {/* Right side */}
+      <Box
+        w={['100%', '100%', '50%', '50%']}
+        borderLeft={['none', 'none', '2px solid', '2px solid']}
+        borderColor={'#BBBBBB'}
+      >
+        <Stack direction={'column'} p={['', '', '40px', '40px']}>
+          {loading ? (
+            <Box>Loading...</Box>
+          ) : (
+            <video src={videoUrl} controls width="100%" />
+          )}
+          <Box>
+            <Img src="/transcript.png" />
+          </Box>
+        </Stack>
+      </Box>
+    </Stack>
+  );
+};
 
 export default Ready;
